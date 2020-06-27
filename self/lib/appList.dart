3ad3cbbae174a80c1adbe4e4ff2d007552823b07
Ashutosh Jha <aj97389@gmail.com>
2020-06-27 12:45:59 +0530

    Added cards on appList

--- self/lib/appList.dart
+++ self/lib/appList.dart
@@ -11,50 +11,75 @@ class _AppListState extends State<AppList> {
     [
       'Facebook',
       'https://www.facebook.com/',
-      'https://upload.wikimedia.org/wikipedia/commons/0/05/Facebook_Logo_%282019%29.png'
+      'https://upload.wikimedia.org/wikipedia/commons/0/05/Facebook_Logo_%282019%29.png',
+      'Facebook, Inc. is an American social media conglomerate corporation based in Menlo Park, California.'
     ],
     [
       'Telegram',
       'https://web.telegram.org/',
-      'https://upload.wikimedia.org/wikipedia/commons/0/05/Facebook_Logo_%282019%29.png'
+      'https://upload.wikimedia.org/wikipedia/commons/0/05/Facebook_Logo_%282019%29.png',
+      'Facebook, Inc. is an American social media conglomerate corporation based in Menlo Park, California.',
     ]
   ];
   @override
   Widget build(BuildContext context) {
     final Map data = ModalRoute.of(context).settings.arguments;
     return Scaffold(
-        appBar: AppBar(),
-        body: Wrap(
-          children: social
-              .map((item) => Padding(
-                  padding: EdgeInsets.all(5.0),
-                  child: GestureDetector(
-                    onTap: (){
-                      print('clicked');
-                    },
-                                      child: Container(
-                      child: Card(
-                        child: Padding(
-                          padding: EdgeInsets.all(10.0),
-                          child: Column(
-                            children: <Widget>[
-                              Image.network(
-                                item[2],
-                                height: 40.0,
-                              ),
-                              SizedBox(
-                                height: 10.0,
-                              ),
-                              Text(item[0])
-                            ],
-                          ),
-                        ),
-                        
+      appBar: AppBar(
+        title: Text(data['category'] == 'social'
+            ? 'Social Apps'
+            : data['category'] == 'news'
+                ? 'News'
+                : data['category'] == 'sports' ? 'Sports' : 'Shopping Apps'),
+      ),
+      body: Wrap(
+        children: social
+            .map(
+              (item) => Card(
+                elevation: 0.0,
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: ListTile(
+                    title: Padding(
+                      padding: EdgeInsets.all(5.0),
+                      child: Text(
+                        item[0],
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontFamily: 'Raleway',
+                            fontWeight: FontWeight.w700),
                       ),
                     ),
-                  )))
-              .toList()
-              .cast<Widget>(),
-        ));
+                    subtitle: Padding(
+                      padding:
+                          EdgeInsets.only(left: 5.0, right: 5.0, bottom: 5.0),
+                      child: Text(
+                        item[3],
+                        style: TextStyle(
+                            color: Colors.black54,
+                            fontSize: 12.0,
+                            fontWeight: FontWeight.w300),
+                      ),
+                    ),
+                    trailing: Icon(Icons.arrow_forward_ios),
+                    leading: Container(
+                      child: Padding(
+                        padding: EdgeInsets.all(0.0),
+                        child: Image(
+                          image: NetworkImage(item[2]),
+                          height: 50.00,
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                          color: Colors.black12, shape: BoxShape.circle),
+                    ),
+                  ),
+                ),
+              ),
+            )
+            .toList()
+            .cast<Widget>(),
+      ),
+    );
   }
 }
--- self/lib/main.dart
+++ self/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:self/home.dart' as home;
 import 'package:self/appList.dart' as appList;
+
 void main() {
   runApp(MyApp());
 }
@@ -10,23 +11,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'Smitty LLC Application',
-          theme: ThemeData(
-            primarySwatch: Colors.grey,
-            primaryColor: Color.fromRGBO(1, 45, 138, 1),
-            brightness: Brightness.light,
-            backgroundColor: Color(0xFFE5E5E5),
-            accentColor: Colors.black,
-            accentIconTheme: IconThemeData(color: Colors.white),
-            dividerColor: Colors.white54,
-          ),
-          home: Material(child: home.Dashboard(),),
-          routes: {
-            '/appList':(context)=>appList.AppList(),
-
-          },
-         
-        );
+      debugShowCheckedModeBanner: false,
+      title: 'Smitty LLC Application',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        primaryColor: Color.fromRGBO(1, 45, 138, 1),
+        brightness: Brightness.light,
+        backgroundColor: Color(0xFFE5E5E5),
+        accentColor: Colors.black,
+        accentIconTheme: IconThemeData(color: Colors.white),
+        dividerColor: Colors.white54,
+      ),
+      home: Material(
+        child: home.Dashboard(),
+      ),
+      routes: {
+        '/appList': (context) => appList.AppList(),
+      },
+    );
   }
 }
