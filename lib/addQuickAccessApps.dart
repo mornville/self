e1d0788017dbef4256a3481ad46cf9aa64e5b157
mornville <aj97389@gmail.com>
2020-07-12 14:27:26 +0530

    Added search functionality for quick access tab

--- lib/addQuickAccessApps.dart
+++ lib/addQuickAccessApps.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:self/appList.dart' as appList;
+import 'package:self/data.dart';
 
 class AddFavourite extends StatefulWidget {
   @override
@@ -7,8 +7,10 @@ class AddFavourite extends StatefulWidget {
 }
 
 class _AddFavouriteState extends State<AddFavourite> {
+  List searchResult = List<Widget>();
   bool showTopSearches = false;
-  var topSearches = List<Widget>();
+  List allApps = news + sports + social + shopping;
+
   TextEditingController searchBarContent = TextEditingController();
   @override
   Widget build(BuildContext context) {
@@ -18,40 +20,90 @@ class _AddFavouriteState extends State<AddFavourite> {
           title: Text('Add Quick Access Apps'),
         ),
         body: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(top: 20.0),
-                    child: TextFormField(
-                      onTap: () {
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top: 20.0),
+                child: TextFormField(
+                  onTap: () {
+                    setState(() {
+                      showTopSearches = true;
+                    });
+                  },
+                  onChanged: (text) {
+                    searchResult = List<Widget>();
+                    for (int i = 0; i < allApps.length; i++) {
+                      if (text == '') {
+                        searchResult = List<Widget>();
+                        break;
+                      }
+                      if (allApps[i][0]
+                          .toLowerCase()
+                          .contains(text.toLowerCase())) {
                         setState(() {
-                          showTopSearches = true;
+                          searchResult.add(Padding(
+                            padding: EdgeInsets.all(3.0),
+                            child: ListTile(
+                              title: Text(allApps[i][0]),
+                              leading: Image.asset(
+                                'assets/appIcons/' + allApps[i][2],
+                                height: 30.0,
+                              ),
+                            ),
+                          ));
                         });
-                      },
-                      decoration: InputDecoration(
-                        hintText: "Search",
-                        fillColor: Colors.white,
-                        filled: true,
-                        suffixIcon: Icon(Icons.search),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20.0),
-                          borderSide: BorderSide(color: Colors.black12),
-                        ),
-                        contentPadding: EdgeInsets.symmetric(
-                            horizontal: 16.0, vertical: 16.0),
-                      ),
+                      }
+                    }
+                  },
+                  decoration: InputDecoration(
+                    hintText: "Search",
+                    fillColor: Colors.white,
+                    filled: true,
+                    suffixIcon: Icon(Icons.search),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                      borderSide: BorderSide(color: Colors.black12),
                     ),
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
                   ),
-                  showTopSearches
-                      ? Padding(
-                          child: Container(height: 100, color: Colors.black12),
-                          padding: EdgeInsets.all(0.0),
-                        )
-                      : Container(),
-                ],
+                ),
               ),
-            )));
+              (showTopSearches && searchResult.isEmpty)
+                  ? Padding(
+                      child: Container(
+                        color: Colors.black12,
+                        child: Column(
+                          children: <Widget>[
+                            ListTile(
+                              title: Text(topApps[0][0]),
+                              leading: Icon(Icons.history),
+                            ),
+                            ListTile(
+                              title: Text(topApps[1][0]),
+                              leading: Icon(Icons.history),
+                            ),
+                            ListTile(
+                              title: Text(topApps[2][0]),
+                              leading: Icon(Icons.history),
+                            ),
+                          ],
+                        ),
+                      ),
+                      padding: EdgeInsets.all(0.0),
+                    )
+                  : Expanded(
+                      child: Container(
+                      color: Colors.white,
+                      child: ListView.builder(
+                          itemCount: searchResult.length,
+                          itemBuilder: (BuildContext ctxt, i) {
+                            return searchResult[i];
+                          }),
+                    ))
+            ],
+          ),
+        ));
   }
 }
